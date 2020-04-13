fcb58b4d004f1a7fa22551e95aba71648a85c205
Remi de Boer <r.d.d.de.boer@hva.nl>
2020-04-13 12:20:17 +0200

    eerste commit: doedel setup

---
+++ src/main/kotlin/main/main.kt
@@ -0,0 +1,65 @@
+package main
+
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.time.Duration
+
+/**
+ * We start here to make request, not in a silly class with a static main, but just MAIN! :D
+ */
+fun main(args: Array<String>) {
+    val options = Options()
+    val parser = DefaultParser()
+
+    options.addOption("u", "url", true, "the requested url")
+    options.addOption("H", false, "prints response headers")
+    options.addOption("h", "help", false, "prints commands")
+    options.addOption("X", "method", true, "the http method: supported methods are GET POST")
+    val cmd = parser.parse(options, args)
+
+    val client = HttpClient.newBuilder()
+        .followRedirects(HttpClient.Redirect.NEVER)
+        .connectTimeout(Duration.ofMillis(1500))
+        .build()
+    val requestBuilder = HttpRequest.newBuilder().version(HttpClient.Version.HTTP_1_1)
+    requestBuilder.header("User-Agent", "kUrl")
+
+    if (cmd.hasOption("H")) {
+        println("Ik hebbe -h")
+    }
+
+    if (cmd.hasOption("u")) {
+        val url = cmd.getOptionValue("u")
+        requestBuilder.uri(URI.create(url))
+    }
+
+    if (cmd.argList.isEmpty() || cmd.hasOption("h")) {
+        val helpFormatter = HelpFormatter()
+        helpFormatter.printHelp("kUrl", options)
+    }
+    if (
+        cmd.hasOption("X")) {
+        when{
+            cmd.getOptionValue("X").equals("GET", true) -> {
+                requestBuilder.GET()
+            }
+            cmd.getOptionValue("X").equals("POST", true) -> {
+                requestBuilder.POST(HttpRequest.BodyPublishers.noBody())
+            }
+        }
+
+    }
+
+    val request = requestBuilder.build()
+
+    println("sending request:\n${request.method()} ${request.uri()} ${request.version().get()}")
+
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+    println(response)
+}
